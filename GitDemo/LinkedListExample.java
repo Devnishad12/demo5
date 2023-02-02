a08339f50a17b2e429255f96829c705ec84207c8
dev <devsami.nishad12@gmail.com>
2023-02-02 15:55:36 +0530

    first commit

---
+++ GitDemo/LinkedListExample.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        linkedList.add(40);
+        
+        System.out.println("Linked List: " + linkedList);
+    }
+}
